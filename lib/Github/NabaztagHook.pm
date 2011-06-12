0248437348c1b28c1cc96bc7b10a5b6b550580f2
Alexis Sukrieh <sukria@sukria.net>
2011-06-12 11:25:14 +0200

    first version of the hook

--- lib/Github/NabaztagHook.pm
+++ lib/Github/NabaztagHook.pm
@@ -8,6 +8,40 @@ This package provides the needed routes and all the Dancer code.
 =cut
 
 use Dancer ':syntax';
+use Nabaztag::Client;
 
+my $_nabaztag;
+
+sub nabaztag {
+    return $_nabaztag if defined $_nabaztag;
+    
+    $_nabaztag = Nabaztag::Client->new(
+        serial => setting('serial'),
+        token  => setting('token')
+    );
+}
+
+post '/' => sub {
+    my $payload = from_json(param('payload'));
+    my $format  = setting('app')->{'message_format'};
+    my $message = _get_message($payload, $format);
+
+    nabaztag->send_message(message => $message);
+};
+
+sub _get_message {
+    my ($payload, $format) = @_;
+
+    my $repo = $payload->{repository}{name};
+    my $commits = $payload->{commits};
+    my %authors = map { $_->{author}{name} => 1 } @$commits;
+    my $authors = join(', ', sort(keys(%authors)));
+
+    my $message = $format;
+    $message =~ s/%a/$authors/g;
+    $message =~ s/%p/$repo/g;
+
+    return $message;
+}
 
 1;
