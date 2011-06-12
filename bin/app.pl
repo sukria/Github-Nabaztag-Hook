aae01b96795da6af6c69a8e6d9116a7f59d6ee47
Alexis Sukrieh <sukria@sukria.net>
2011-06-12 10:18:33 +0200

    Dancer scaffold

---
+++ bin/app.pl
@@ -0,0 +1,4 @@
+#!/usr/bin/env perl
+use Dancer;
+use Github::NabaztagHook;
+dance;
---
+++ lib/Github/NabaztagHook.pm
@@ -0,0 +1,13 @@
+package Github::NabaztagHook;
+# ABSTRACT: root package for the Nabaztag webhook routes
+
+=head1 DESCRIPTION
+
+This package provides the needed routes and all the Dancer code.
+
+=cut
+
+use Dancer ':syntax';
+
+
+1;
